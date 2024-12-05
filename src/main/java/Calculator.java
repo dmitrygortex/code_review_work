aa6e967b2b84400c303b24e3dc672153d863fff3
dmitrygortex <gortex@internet.ru>
2024-12-05 14:28:01 +0300

    first commit by dmitry gortex

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,24 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if(b == 0){
+            throw new IllegalArgumentException("zero division");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        // (5 + 5) * (11 - 1) / 25
+        int step1 = add(5, 5);
+        int step2 = dif(11, 1);
+        int step3 = times(step1, step2);
+        return div(step3, 25);
     }
 }
